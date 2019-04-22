21afba25ef43a73ef71315890e7fbc4529890956
songyp <670459249@qq.com>
2019-04-23 00:56:58 +0800

    头部布局

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import indexStyle from './index.less'
-import { Icon, Layout } from 'antd'
+import { Menu, Icon, Layout } from 'antd'
 const { Sider, Content } = Layout
 
 function BasicLayout ({global, dispatch, children }){
+    const handleInfoClick = () => {
+      console.log('info click')
+    }
     return (
       <Layout>
         <div className={indexStyle['basic-header']}>
-          ss
+          <div>
+          <Menu
+            mode="horizontal"
+            defaultSelectedKeys={['home']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="home">个人主页</Menu.Item>
+            <Menu.Item key="weibo">微博</Menu.Item>
+            <Menu.Item key="blog">博客</Menu.Item>
+          </Menu>
+          </div>
+          <div className={indexStyle['right']} onClick={handleInfoClick}>
+            <span className={indexStyle['logo']}></span>
+            <span className={indexStyle['drop']}></span>
+          </div>
         </div>
         <Layout className={indexStyle['basic-content-layout']}>
           <Sider className={indexStyle['basic-sider']}>
